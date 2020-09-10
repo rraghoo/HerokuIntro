b34ff1b703f8ac4526db47468f1037c1534d71a7
Roxy <rraghoo@protonmail.com>
2020-09-10 18:42:08 -0500

    commit

--- main.py
+++ main.py
@@ -6,8 +6,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
- #   some_text = "Message from the handler"
-    return render_template("index.html")
+    some_text = "Message from the handler."
+    current_year = datetime.datetime.now().year
+
+    cities = ["Boston", "Vienna", "Paris", "Berlin"]
+
+    return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
 
 @app.route("/about-me")
 def about_me():
