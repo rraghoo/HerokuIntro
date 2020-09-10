327ac9431ffca14a253d0ab1b4a4a43880304b10
Roxy <rraghoo@protonmail.com>
2020-09-10 18:18:50 -0500

    Initial commit

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+#import datetime
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world!!"
+ #   some_text = "Message from the handler"
+    return render_template("index.html")
+
+@app.route("/about-me")
+def about_me():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run()
