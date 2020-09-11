84d5556fdfa92eb410facaf70f0d5d1600be46e0
Roxy <rraghoo@protonmail.com>
2020-09-10 19:08:03 -0500

    adding post

--- main.py
+++ main.py
@@ -17,5 +17,17 @@ def index():
 def about():
     return render_template("about.html")
 
+@app.route("/contact", methods=["POST"])
+def contact():
+    contact_name = request.form.get("contact-name")
+    contact_email = request.form.get("contact-email")
+    contact_message = request.form.get("contact-message")
+
+    print(contact_name)
+    print(contact_email)
+    print(contact_message)
+
+    return render_template("success.html")
+
 if __name__ == '__main__':
     app.run()
