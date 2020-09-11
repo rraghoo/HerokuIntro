ae870ccccc184e52ee56465b03a50a7137b45dbc
Roxy <rraghoo@protonmail.com>
2020-09-10 19:23:42 -0500

    adding post

--- main.py
+++ main.py
@@ -10,12 +10,22 @@ def index():
     current_year = datetime.datetime.now().year
 
     cities = ["Boston", "Vienna", "Paris", "Berlin"]
-
     return render_template("index.html", some_text=some_text, current_year=current_year, cities=cities)
 
-@app.route("/about-me", methods=["GET"])
+@app.route("/about-me", methods=["GET", "POST"])
 def about():
-    return render_template("about.html")
+    if request.method == "GET":
+        return render_template("about.html")
+    elif request.method == "POST":
+        contact_name = request.form.get("contact-name")
+        contact_email = request.form.get("contact-email")
+        contact_message = request.form.get("contact-message")
+
+        print(contact_name)
+        print(contact_email)
+        print(contact_message)
+
+        return render_template("success.html")
 
 @app.route("/contact", methods=["POST"])
 def contact():
