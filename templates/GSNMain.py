a5d5920e74214433ee4013dcc29a93624e467d9a
Roxy <rraghoo@protonmail.com>
2020-09-14 18:43:45 -0500

    made changes

--- templates/GSNMain.py
+++ templates/GSNMain.py
@@ -3,21 +3,32 @@ from flask import Flas, render_template, request, make_response
 
 app = Flask(__name__)
 
-@app.route("/" methods=["GET"])
+@app.route("/", methods=["GET"])
 def index():
-    #check if there is a cookie named secret number
-    secret_number = request.cookies.get("secret_number")
+    secret_number = request.cookies.get("secret_number")  # check if there is already a cookie named secret_number
 
     response = make_response(render_template("index.html"))
-
-    #If there isn't any such cookie, create a new cookie with this name.
-    If not secret number:
+    if not secret_number:  # if not, create a new cookie
         new_secret = random.randint(1, 30)
-        request.setcookie("secret_number", str(new_secret))
+        response.set_cookie("secret_number", str(new_secret))
 
     return response
 
+
 @app.route("/result", methods=["POST"])
 def result():
-    guess
+    guess = int(request.form.get("guess"))
+    secret_number = int(request.cookies.get("secret_number"))
+
+    if guess == secret_number:
+        message = "Correct! The secret number is {0}".format(str(secret_number))
+        response = make_response(render_template("result.html", message=message))
+        response.set_cookie("secret_number", str(random.randint(1, 30)))  # set the new secret number
+        return response
+    elif guess > secret_number:
+        message = "Your guess is not correct... try something smaller."
+        return render_template("result.html", message=message)
+    elif guess < secret_number:
+        message = "Your guess is not correct... try something bigger."
+        return render_template("result.html", message=message
 
